78059e2bcc0a97d08408f9a6050a8a70296d9102
Tristan Ramirez <tristan.rami@gmail.com>
2020-06-14 17:56:37 +0200

    feat: add useInterval hook

--- example/src/App.js
+++ example/src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { UseDebounce } from './UseDebounce'
 import { UseEventListener } from './UseEventListener'
 import { UseInput } from './UseInput'
+import { UseInterval } from './UseInterval'
 import { UseLocalStorage } from './UseLocalStorage'
 import { UseLockBodyScroll } from './UseLockBodyScroll'
 
@@ -11,6 +12,7 @@ const App = () => {
       <UseDebounce />
       <UseEventListener />
       <UseInput />
+      <UseInterval />
       <UseLocalStorage />
       <UseLockBodyScroll />
     </>
